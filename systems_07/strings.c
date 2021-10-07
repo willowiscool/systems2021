c7764e2a627ef0a4339f042eac71643588ae2f92
Victor Veytsman <vityavv@gmail.com>
2021-10-07 11:20:14 -0400

    Redo strcmp (misunderstood)

--- systems_07/strings.c
+++ systems_07/strings.c
@@ -46,19 +46,15 @@ char *mystrncat(char *dest, char *source, int n) {
 }
 
 int mystrcmp(char *s1, char *s2) {
-	int s1sum = 0;
-	while (*s1) {
-		// googled "treat char as unsigned"
-		// used this result: https://wiki.sei.cmu.edu/confluence/display/c/STR34-C.+Cast+characters+to+unsigned+char+before+converting+to+larger+integer+sizes
-		s1sum += (unsigned char)*s1;
+	while (*s1 && *s2) {
+		if (*s1 > *s2) return 1;
+		if (*s1 < *s2) return -1;
 		s1++;
-	}
-	int s2sum = 0;
-	while (*s2) {
-		s2sum += (unsigned char)*s2;
 		s2++;
 	}
-	return s1sum - s2sum;
+	if (*s1) return 1;
+	if (*s2) return -1;
+	return 0;
 }
 
 char *mystrchr(char *s, char c) {
